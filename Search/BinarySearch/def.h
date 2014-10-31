aee67f2b485304cfae75bbb772eb9fc467f10f20
zkw <zhangkewei_2014@163.com>
2014-10-31 20:34:53 +0800

    实现折半查找

--- Search/BinarySearch/def.h
+++ Search/BinarySearch/def.h
@@ -19,7 +19,7 @@ typedef enum {false, true} bool;
 typedef bool(*COMPARE_METHOD)(int, int);
 
 bool small_to_big(int a, int b);
-void big_to_small(int a, int b);
+bool big_to_small(int a, int b);
 void swap(int *a, int *b);
 void init_arr(int *arr, unsigned int len);
 void print_arr(const int *arr, unsigned int len);
--- Search/BinarySearch/func.c
+++ Search/BinarySearch/func.c
@@ -11,7 +11,7 @@ bool small_to_big(int a, int b)
 	return a <= b;
 }
 
-void big_to_small(int a, int b)
+bool big_to_small(int a, int b)
 {
 	return a >= b;
 }
@@ -56,14 +56,26 @@ void bubble_sort(int *arr, unsigned int len, COMPARE_METHOD compare_method)
 
 int binary_search(const int *arr, unsigned int len, int search_data) //找到返回下标，找不到，返回-1
 {
-
+	int left = 0, right = len-1;
+	int middle = (left + right)/2;
+	while ( (left < right) && (arr[middle] != search_data) ) {
+		if (search_data < arr[middle]) { //在左半部分找
+			right = middle -1;
+		}
+		if (search_data > arr[middle]) { //在右半部分找
+			left = middle + 1;
+		}
+		middle = (left + right)/2;
+	}
+	if (arr[middle] == search_data)
+		return middle;
 	return -1;
 }
 
 void give_evaluation(const int *arr, unsigned int len, int find_pos)
 {
 	if (-1 == find_pos) {
-		printf("not find\n");
+		printf("not found\n");
 		return;
 	}
 	assert( (find_pos >= 0) && (find_pos <= len-1) );
